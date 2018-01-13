e41b28ce3f4fa594f3565ee6bb5da47305d6402b
Datagit <datagit@yahoo.com>
2018-01-13 17:18:14 +0700

    Using service logger in service GreetingGenerator
     On branch master
     Changes to be committed:
    	modified:   .idea/workspace.xml
    	modified:   src/GreetingGenerator.php

--- src/GreetingGenerator.php
+++ src/GreetingGenerator.php
@@ -1,13 +1,22 @@
 <?php
 // src/GreetingGenerator.php
 namespace App;
+use Psr\Log\LoggerInterface;
 
 class GreetingGenerator
 {
+    private $logger;
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function getRandomGreeting()
     {
         $greetings = ['Hey', 'Yo', 'Aloha'];
         $greeting = $greetings[array_rand($greetings)];
+
+        $this->logger->info('Write log in service: Using the greeting: '.$greeting);
 
         return $greeting;
     }
